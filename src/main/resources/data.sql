40dbcd14bfb1e9a393f902c2d7484b5c371a54cc
auskov77 <auskov77@yandex.ru>
2022-05-18 21:31:31 +0300

    spring boot jdbc foundations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(id, fio, age) VALUES (1, 'Ivanov', 20);
+INSERT INTO students(id, fio, age) VALUES (2, 'Petrov', 21);
+INSERT INTO students(id, fio, age) VALUES (3, 'Sidorov', 22);
+INSERT INTO students(id, fio, age) VALUES (4, 'Kim', 25);
